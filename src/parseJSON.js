e9ed428c8fde031e8805ffd98134b5ad77e689e5
Camille Roussel <camille@rousselfamily.com>
2014-12-13 18:27:39 -0500

    parse array inital

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -6,6 +6,32 @@ var parseJSON = function(json) {
   var index;
   var tokens = json.split(/((?:"(?:[^"\\]|\\.)*")|:|,|{|}|[|])/);
 
+  var parseArray = function(index) {
+    var results = [];
+    index = index ? index : 0;
+
+    tokens.splice(index, 1);
+    while (index < tokens.length && tokens[index] !== ']') {
+      if (tokens[index] === '[') {
+        parseArray(index);
+      } else if (tokens[index] === '{') {
+        pareObject(index);
+      } else if (tokens[index] === ',') {
+        tokens.splice(index, 1);
+      } else {
+        results.push(tokens[index]);
+        tokens.splice(index, 1;)
+      }
+    }
+    tokens.splice(index, 1);
+
+    return results;
+  };
+
+  var parseObject = function() {
+
+  };
+
   for (index = 0; index < tokens.length; index += 1) {
     tokens[index] = tokens[index].trim();
   }
@@ -16,9 +42,12 @@ var parseJSON = function(json) {
 
   if (tokens[0] === '[' && tokens[tokens.length-1] === ']') {
     console.log('array branch');
+    //parse array
 
   } else if (tokens[0] === '{' && tokens[tokens.length-1] === '}') {
     console.log('object branch');
+    //parseObject
+
   } else {
     throw new TypeError('invalid JSON');
   }
